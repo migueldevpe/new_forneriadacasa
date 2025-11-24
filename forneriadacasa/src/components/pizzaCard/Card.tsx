4cd3f5c9b5e39150c209fd3b720f8ba47fc47382
Miguel Soares <miguelsoaresabs7@gmail.com>
2025-11-24 19:47:03 -0300

    fix: correção na imagem (SEO)

--- forneriadacasa/src/components/pizzaCard/Card.tsx
+++ forneriadacasa/src/components/pizzaCard/Card.tsx
@@ -61,7 +61,7 @@ export default function Card({ img="https://placehold.co/225x200/cdcdcd/000?text
     <div className="card-container grid gap-2.5 bg-white w-full !p-2 rounded-md">
       <div className="relative rounded-md border-2 overflow-hidden" style={{ borderColor: `${border_color}` } as React.CSSProperties}>
         {img_ia && (
-          <img src={iaWatermarker} className={`absolute aspect-[1024/1024] w-30 pointer-events-none [transform:_translateX(-50%)] left-6/12 bottom-[-24px]`} alt="Essa imagem foi gerado por IA." title="Essa imagem foi gerado por IA." loading="lazy" decoding="async" fetchPriority="low" />
+          <img src={iaWatermarker} className={`absolute aspect-[512/256] w-30 pointer-events-none [transform:_translateX(-50%)] left-6/12 bottom-[-24px]`} alt="Essa imagem foi gerado por IA." title="Essa imagem foi gerado por IA." loading="lazy" decoding="async" fetchPriority="low" />
         )}
         {label && (
           <span className={`absolute top-1 right-1 text-white text-xs font-bold rounded-md !p-1`} style={{ background: `${bg_label}` } as React.CSSProperties}>
